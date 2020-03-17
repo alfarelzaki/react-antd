a72c11036529e9454b9b83bcdfc19065debaca31
Alfarel Zaki <alfarell.zaki@mail.com>
2020-03-17 16:10:33 +0700

    Updates

---
+++ precache-manifest.9c7b8411b62dd110fa2f24dad7c39511.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24363b170f4aa6b46f12bb3ec1a544dc",
+    "url": "/furnitura-react/index.html"
+  },
+  {
+    "revision": "85ee30dc65ec68105832",
+    "url": "/furnitura-react/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "c4da10b6b04233ef0294",
+    "url": "/furnitura-react/static/css/main.88c9f9d9.chunk.css"
+  },
+  {
+    "revision": "85ee30dc65ec68105832",
+    "url": "/furnitura-react/static/js/2.2ceda658.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/furnitura-react/static/js/2.2ceda658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4da10b6b04233ef0294",
+    "url": "/furnitura-react/static/js/main.234abda0.chunk.js"
+  },
+  {
+    "revision": "6ef8765cbe9770c2ddf9",
+    "url": "/furnitura-react/static/js/runtime-main.00f65b3a.js"
+  },
+  {
+    "revision": "1c52cb742309acfda87d2eafea403a57",
+    "url": "/furnitura-react/static/media/furniture1.1c52cb74.jpg"
+  },
+  {
+    "revision": "bd5924c3cfcd6ee760da54ca44d5108d",
+    "url": "/furnitura-react/static/media/furniture2.bd5924c3.jpg"
+  },
+  {
+    "revision": "e8399f077974b3072a3c394c0c9e7d47",
+    "url": "/furnitura-react/static/media/furniture3.e8399f07.jpg"
+  },
+  {
+    "revision": "ac1fa5f744958e3f028f24fc9f6c8841",
+    "url": "/furnitura-react/static/media/furniture4.ac1fa5f7.jpg"
+  },
+  {
+    "revision": "a6fcd36ddf568e277ca93bf40f16e7b3",
+    "url": "/furnitura-react/static/media/point1.a6fcd36d.jpg"
+  },
+  {
+    "revision": "00da7c8eb15b560db234187d4abb9e7f",
+    "url": "/furnitura-react/static/media/point2.00da7c8e.jpg"
+  },
+  {
+    "revision": "bad3b56c3d6d042c086c3cdac8b5f168",
+    "url": "/furnitura-react/static/media/point3.bad3b56c.jpg"
+  }
+]);
